c8d7dd241f3c1cae7a6e8fd6b784f8dcca6a8fe6
EG-easy <1018eguchi@gmail.com>
2019-10-03 16:11:26 +0900

    fix

--- install.sh
+++ install.sh
@@ -1,31 +1,7 @@
 #!/bin/sh
-DOTPATH=~/.dotfiles
+DOTPATH=~/dotfiles
 
-# git が使えるなら git
-if has "git"; then
-    git clone --recursive "$GITHUB_URL" "$DOTPATH"
-
-# 使えない場合は curl か wget を使用する
-elif has "curl" || has "wget"; then
-    tarball="https://github.com/EG-easy/dotfiles/archive/master.tar.gz"
-
-    # どっちかでダウンロードして，tar に流す
-    if has "curl"; then
-        curl -L "$tarball"
-
-    elif has "wget"; then
-        wget -O - "$tarball"
-
-    fi | tar zxv
-
-    # 解凍したら，DOTPATH に置く
-    mv -f dotfiles-master "$DOTPATH"
-
-else
-    die "curl or wget required"
-fi
-
-cd ~/.dotfiles
+cd ~/dotfiles
 if [ $? -ne 0 ]; then
     die "not found: $DOTPATH"
 fi
